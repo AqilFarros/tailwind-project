0de337fa94b494266115b5b78e54185db79d1131
Aqil Farros <aqilghonim@gmail.com>
2024-02-02 10:01:33 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,24 @@ module.exports = {
   ],
   theme: {
     extend: {
-      textStrokeWidth: {
-        '1': '1px',
-        '40': '40px'
-      },
-      textStrokeColor: theme => theme('colors'),
+      keyframes: {
+        ajubAjub: {
+          '30%, 38%': {
+            boxShadow: '0 0 20px 5px #4c1d95',
+            backgroundColor: 'black'
+          },
+
+          '40%, 90%': {
+            boxShadow: '0 0 5px 5px #4c1d95',
+            backgroundColor: 'black'
+          },
+
+          '100%, 0%, 25%': {
+            boxShadow: '0 0 0 0',
+            backgroundColor: '#3f3f46'
+          }
+        },
+      }
     },
   },
   plugins: [
